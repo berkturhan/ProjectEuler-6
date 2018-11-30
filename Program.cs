dbd1405f3be7bd95fbfd2aa4a12aeee3ebe01536
berkturhan <31630169+berkturhan@users.noreply.github.com>
2018-11-30 17:59:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEuler6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            long sum1 = 0;
+            long sum2 = 0;
+
+            for(int i=1; i <=n; i++)
+            {
+                sum1 = sum1 + (i * i);
+            }
+
+            sum2 = Convert.ToInt64(Math.Pow((n * (n + 1)) / 2, 2));
+
+
+            Console.WriteLine("Square of the sum of the first one hundred natural numbers: " + sum2);
+            Console.WriteLine("Sum of the squares of the first one hundred natural numbers: " + sum1);
+            Console.WriteLine("The difference: " + (sum2-sum1));
+            Console.ReadLine();
+        }
+    }
+}
